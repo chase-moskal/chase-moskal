58f8f54cb81fba2cd01dbb27024ba507d7e320b2
Chase Moskal <chasemoskal@gmail.com>
2026-09-05 00:03:35 -0700

    fresh: scute ssg and site imagery

--- s/index.html.ts
+++ s/index.html.ts
@@ -1,54 +1,61 @@
 
-import {ssg, html} from "@e280/scute"
+import {template, html, socialCard} from "@e280/scute"
 
 const title = "CHASE MOSKAL"
 const domain = "chasemoskal.com"
-const favicon = "/assets/holo.png"
+const favicon = "/assets/c.png"
 
-export default ssg.page(import.meta.url, async _orb => ({
-	title: "CHASE MOSKAL",
-	js: "main.bundle.min.js",
-	css: "style.css",
-	dark: true,
-	favicon,
+export default template(import.meta.url, async orb => html`
+	<!doctype html>
+	<html>
+		<head>
+			<meta charset="utf-8"/>
+			<meta name="viewport" content="width=device-width,initial-scale=1"/>
+			<meta name="darkreader-lock"/>
+			<style>@layer base{html{background:#000}}</style>
 
-	head: html`
-		<link rel="preconnect" href="https://fonts.googleapis.com">
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@900&family=Share+Tech&display=swap" rel="stylesheet">
-	`,
+			<title>CHASE MOSKAL</title>
+			<link rel="icon" href="${orb.hashurl(favicon)}"/>
 
-	socialCard: {
-		themeColor: "#ff7700",
-		siteName: domain,
-		title,
-		description: "unrelenting buildercore maniac",
-		image: `https://${domain}${favicon}`,
-		url: `https://${domain}/`,
-	},
+			${socialCard({
+				title,
+				siteName: domain,
+				themeColor: "#ff7700",
+				url: `https://${domain}/`,
+				image: `https://${domain}${favicon}`,
+				description: "relentless buildercore maniac",
+			})}
 
-	body: html`
-		<h1>
-			<strong>CHASE MOSKAL</strong>
-			<span class=version>v2.0.0-1</span>
-		</h1>
+			<link rel="stylesheet" href="${orb.hashurl("style.css")}"/>
+			<script type="module" src="${orb.hashurl("main.bundle.min.js")}"></script>
 
-		<nav>
-			<a href="https://github.com/chase-moskal">github</a>
-			<a href="https://e280.org/">e280</a>
-			<a href="https://authlocal.org/">authlocal</a>
-			<a href="https://benevolent.games/">benevolent</a>
-			<a href="/assets/fastfetch.png">arch btw</a>
-		</nav>
+			<link rel="preconnect" href="https://fonts.googleapis.com"/>
+			<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+			<link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@900&family=Share+Tech&display=swap" rel="stylesheet"/>
+		</head>
+		<body>
+			<h1>
+				<strong>CHASE MOSKAL</strong>
+				<span class=version>v2.0.0-1</span>
+			</h1>
 
-		<div class=plate>
-			<img class=avatar alt="" src="/assets/chase.avif"/>
-			<blockquote>
-				<p><strong>A cheap laptop is all you need to touch the lives of millions.</strong></p>
-				<p>If only you knew how to type the right sequence of keystrokes.</p>
-				<p>Too bad software is the hardest thing in the world.</p>
-			</blockquote>
-		</div>
-	`,
-}))
+			<nav>
+				<a href="https://github.com/chase-moskal">github</a>
+				<a href="https://e280.org/">e280</a>
+				<a href="https://authlocal.org/">authlocal</a>
+				<a href="https://benevolent.games/">benevolent</a>
+				<a href="/assets/fastfetch.png">arch btw</a>
+			</nav>
+
+			<div class=plate>
+				<img class=avatar alt="" src="/assets/chase.webp"/>
+				<blockquote>
+					<p><strong>A cheap laptop is all you need to touch the lives of millions.</strong></p>
+					<p>If only you knew how to type the right sequence of keystrokes.</p>
+					<p>Too bad software is the hardest thing in the world.</p>
+				</blockquote>
+			</div>
+		</body>
+	</html>
+`)
 
